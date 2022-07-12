f3423fe12b389d0d3b242eadc9a26711074c8c26
raekuul <raekuul@github.com>
2022-07-12 14:56:15 -0500

    split games to separate folders; also Kriegspire well

---
+++ mm6-mandate/Maps/outb1.lua
@@ -0,0 +1,5 @@
+-- remove the gold-to-experience and +30 Level wells
+
+
+Game.MapEvtLines:RemoveEvent(101) -- removes the Gold-to-Experience well
+Game.MapEvtLines:RemoveEvent(102) -- removes the +30 Level well
